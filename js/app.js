9f8140690cfa9d83dbdde5fd085dbfc2fcf2c508
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-09-18 17:57:22 +0100

    game board MAK3: clickevents working, boxes not appearing yet

--- js/app.js
+++ js/app.js
@@ -3,35 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const game = {};
 //===== Game Board ====//
   game.board = document.getElementById('board');
+  game.playerTurn = document.getElementById('playerturn');
+  game.turn = true;
   game.plane = document.createElement('div');
 
   game.board.appendChild(game.plane);
 
-  game.rows = 11;
-  game.cols = 11;
+  game.boxesArray = [];
+  game.v_linesArray = [];
+  game.h_linesArray = [];
 
-
-  game.rowsArray = [];
-  game.tilesArray = [];
-  //
-  // for (var i = 0; i < game.rows; i++) {
-  //   // ROW
-  //   game.tr = document.createElement('div');
-  //   game.rowsArray.push(game.tr);
-  //   for (var k = 0; k < game.rowsArray.length; k++) {
-  //     if (k % 2 === 0) {
-  //       game.rowsArray[k].setAttribute('class', 'dot')
-  //     }
-  //   }
-  //   game.area.appendChild(game.tr);
-  //
-  //   for (var j = 0; j < game.cols; j++) {
-  //     game.td = document.createElement('div');
-  //     game.td.setAttribute('class', 'cols');
-  //     game.tilesArray.push(game.td);
-  //     game.tr.appendChild(game.td);
-  //   }
-  // }//close dynamic grid
 
   // Create 10 rows
   // Even (starting at 0) should be DOT -> H_LINE
@@ -50,7 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (j % 2 === 0) {
           col.setAttribute('class', 'dot')
         } else {
-          col.setAttribute('class', 'h_line')
+          col.setAttribute('class', 'h_line');
+          game.h_linesArray.push(col);
         }
         row.appendChild(col)
       }
@@ -58,9 +40,13 @@ document.addEventListener('DOMContentLoaded', () => {
       for (var j = 0; j < 11; j++) {
         const col = document.createElement('div');
         if (j % 2 === 0) {
-          col.setAttribute('class', 'v_line')
+          col.setAttribute('class', 'v_line');
+          game.v_linesArray.push(col);
         } else {
           col.setAttribute('class', 'box')
+          col.setAttribute('data-col', `${j}`);
+          col.setAttribute('data-row', `${i}`);
+          game.boxesArray.push(col);
         }
         row.appendChild(col)
       }
@@ -68,19 +54,71 @@ document.addEventListener('DOMContentLoaded', () => {
     game.plane.appendChild(row);
   }
 
-  // game.plane.appendChild(game.area);
+  //===Game Board actions ===//
 
-  // console.log(game.rowsArray[0].attributes[0].value);
+  //click line, line gets current player class.
+  //click line and the boxes adjacent data num value ++
+  // when box data num value = 4, then box gets current player class and player turn repeats.
+
+  game.setClicks = () => {
+    const boxes = document.getElementsByClassName('box');
+    for (var i = 0; i < boxes.length; i++) {
+      boxes[i].addEventListener('click', (e) => {
+        console.log(e.target.getAttribute('data-col'));
+        console.log(e.target.getAttribute('data-row'));
+      })
+    }
+    // HORIZONTAL LINES
+    for (var i = 0; i < game.h_linesArray.length; i++) {
+      game.h_linesArray[i].addEventListener('click', function (e) {
+        if (e.target.innerHTML.length === 0) {
+          if (game.turn) {
+            game.playerTurn.innerText = "It's Player Y's Turn!";
+            e.target.setAttribute('class', 'h_line playerY');
+            game.turn = false;
+          }else {
+            game.playerTurn.innerText = "It's Player X's Turn!";
+            e.target.setAttribute('class', 'h_line playerX');
+            game.turn = true;
+          };
+        };
+      });
+    };
+    // VERTICAL LINES
+    for (var i = 0; i < game.v_linesArray.length; i++) {
+      game.v_linesArray[i].addEventListener('click', function (e) {
+        if (e.target.innerHTML.length === 0) {
+          if (game.turn) {
+            game.playerTurn.innerText = "It's Player Y's Turn!";
+            e.target.setAttribute('class', 'v_line playerY');
+            game.turn = false;
+          }else {
+            game.playerTurn.innerText = "It's Player X's Turn!";
+            e.target.setAttribute('class', 'v_line playerX');
+            game.turn = true;
+          };
+        };
+      });
+    };
+  };//close setClicks
+
+  game.setClicks();
+
+  console.log(game.boxesArray[0].attributes[2].value);
+  
 
 
 
-  //===Game Board Selectors ===//
 
 
 
 
-  // game.boxesPlayerX= [];
-  // game.boxesPlayerY= [];
+
+
+
+
+
+
 
 
 
